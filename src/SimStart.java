7781e1fe4333274f7cc07074e2ae106befb1be42
ZeTaaT <78171648+ZeTaaT@users.noreply.github.com>
2022-02-04 13:55:18 +0000

    gogog

---
+++ src/SimStart.java
@@ -0,0 +1,16 @@
+
+public class SimStart {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		Grid cellGrid = new Grid(40, 25);
+		populate(cellGrid);
+		
+		cellGrid.show();
+		
+		
+		
+	}
+}
